4827d62ee2caa47baf7195ec02c97ba9275d27a9
Matheus Guedes <mguedes@arclight.dev>
2020-10-12 15:36:11 -0300

    Initial Commit

---
+++ app/src/main/java/dev/arclight/weatherwarn/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.arclight.weatherwarn.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.arclight.weatherwarn.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
